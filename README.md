cbd62c8ffea3ff4e9fe0c37594f335ba2d3fb9b0
RanaUniverse <RanaUniverse321@gmail.com>
2024-11-24 09:16:46 +0530

    Make simple Flask App

---
+++ README.md
@@ -0,0 +1,13 @@
+# Information About This Flask
+
+
+Important Part Below Image 👇👇👇
+![Available This Page in my mobile in same wifi](files/images/2_open_web_in_mobile.png)
+This is the main part which allow to see the page in my mobile in same network.
+
+
+
+
+This is i am learning flask from beginning,
+
+![Making This Project](files/images/1_how_i_started.png)
---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+This is simple flask just in this main.py file
+"""
+
+import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+name = "Rana Universe"
+
+
+@app.route("/")
+def hello():
+    now_time_ist = datetime.datetime.now(
+        datetime.timezone(datetime.timedelta(hours=5, minutes=30))
+    )
+    return f"{name.upper()}<br>Current Time in India is: {now_time_ist}"
+
+
+@app.route("/about")
+def about():
+    return f"This is the About page."
+
+
+@app.route("/contact")
+def contact_info():
+    text = f"Hello i am {name.upper()} Please Mail me at: <br>RanaUniverse321@gmail.com"
+    return text
+
+
+if __name__ == "__main__":
+    # app.run(debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
